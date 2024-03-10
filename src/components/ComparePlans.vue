e2fea92dc3b23980c1c6d32025f86b9f6b2e7832
im-fahad <abdullahalfahad.bd@gmail.com>
2024-03-10 22:14:31 +0600

    compare plans cards added

---
+++ src/components/ComparePlans.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const currentPlan = ref('Premium')
+
+const plans = [
+  {
+    name: 'Basic',
+    price: 0,
+    title: 'It’s Free, Try out skills-based hiring From Now.',
+    features: [
+      '5 Assessment Invitations Per Month',
+      'Custom Test Configuration',
+      'Change Plan for extra Candidates',
+      'Realistic Simulation Tests',
+      'Standard Test Library',
+      'Validated Content',
+      '24/7 Live Support'
+    ]
+  },
+  {
+    name: 'Premium',
+    price: 100,
+    title: 'Great for Medium teams looking to hire best talent, faster.',
+    features: [
+      '100 Assessment Invitations Per Year',
+      '$1 for an extra Invitation',
+      'Custom Test Configuration',
+      'Change Plan for extra Candidates',
+      'Realistic Simulation Tests',
+      'Standard Test Library',
+      'Validated Content',
+      '24/7 Live Support',
+      'Custom Content with Editor',
+      'Customized Branding'
+    ]
+  },
+  {
+    name: 'Business',
+    price: 450,
+    title: 'Great for Medium teams looking to hire best talent, faster.',
+    features: [
+      '500 Assessment Invitations Per Year',
+      '$1 for an extra Invitation',
+      'Custom Test Configuration',
+      'Change Plan for extra Candidates',
+      'Realistic Simulation Tests',
+      'Standard Test Library',
+      'Validated Content',
+      '24/7 Live Support',
+      'Custom Content with Editor',
+      'Customized Branding'
+    ]
+  }
+]
+</script>
+
+<template>
+  <div class="compare-plans">
+    <div class="compare-plans__head">
+      <h1 class="compare-plans__title text-lg">Compare plans</h1>
+      <p class="compare-plans__desc text-xxs">
+        We’ll credit your account if you need to downgrade during the billing cycle.
+      </p>
+    </div>
+
+    <div class="plans">
+      <div
+        v-for="(plan, key) in plans"
+        class="plan"
+        :class="currentPlan === plan.name ? 'active' : ''"
+        :key="key"
+      >
+        <div class="plan__head">
+          <h2 class="plan__title" v-text="plan.name" />
+          <h2 class="plan__label">{{ plan.price }} USD /Month</h2>
+        </div>
+        <div class="plan__inner">
+          <h5 class="plan__subtitle" v-text="plan.title" />
+          <ul class="plan__features">
+            <li
+              class="plan__features-item"
+              v-for="(feature, key) in plan.features"
+              :key="key"
+              v-text="feature"
+            />
+          </ul>
+        </div>
+
+        <button
+          class="plan-button"
+          :disabled="currentPlan === plan.name"
+          v-if="plan.name !== 'Basic'"
+        >
+          {{ currentPlan === plan.name ? 'Current' : 'Upgrade' }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import ManageSubscription from '@/components/ManageSubscription.vue'
+import ComparePlans from '@/components/ComparePlans.vue'
 
 const activeTab = ref('subscription')
 
@@ -7,16 +9,6 @@ const tabs = {
   subscription: 'Manage Subscription',
   plans: 'Compare Plans'
 }
-
-const features = [
-  '5 Assessment Invitations Per Month',
-  'Custom Test Configuration',
-  'Change Plan for extra Candidates',
-  'Realistic Simulation Tests',
-  'Standard Test Library',
-  'Validated Content',
-  '24/7 Live Support'
-]
 
 const manageTab = (tab: string) => {
   activeTab.value = tab
@@ -36,113 +28,8 @@ const manageTab = (tab: string) => {
       />
     </ul>
 
-    <div class="tt-tab__inner">
-      <section class="tt-section">
-        <div class="tt-section__head">
-          <h1 class="tt-section__title text-sm">Current Plan</h1>
-        </div>
+    <ManageSubscription v-if="activeTab === 'subscription'" />
 
-        <div class="current-plan">
-          <div class="current-plan__left">
-            <div class="current-plan__info">
-              <h2 class="current-plan__name text-lg">Basic</h2>
-              <h3 class="current-plan__label text-xs">0 USD /Month</h3>
-            </div>
-
-            <div class="hr" />
-
-            <ul class="plan-features">
-              <li
-                class="plan-features__item"
-                v-for="(feature, key) in features"
-                :key="key"
-                v-text="feature"
-              />
-            </ul>
-          </div>
-          <div class="current-plan__right">
-            <div class="current-plan__count text-lg">5</div>
-            <h4 class="current-plan__title text-xs">Current Invitations Limit</h4>
-            <h5 class="current-plan__desc text-xxs">Expired On : 6th September, 2024</h5>
-          </div>
-        </div>
-
-        <button class="upgrade-button">Upgrade Plan</button>
-      </section>
-
-      <section class="tt-section">
-        <div class="tt-section__head">
-          <h1 class="tt-section__title text-sm">Recharge History</h1>
-          <button class="download-all text-xxs">
-            <span class="icon icon-Download" />
-            Download All
-          </button>
-        </div>
-
-        <table class="tt-table" cellspacing="0" cellpadding="0">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Amount</th>
-              <th>Plan</th>
-              <th>Payment Type</th>
-              <th>Transaction ID</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>28th Nov, 2021</td>
-              <td>2000 BDT</td>
-              <td>Basic</td>
-              <td>bKash</td>
-              <td>1254789965653</td>
-              <td>
-                <span class="status status--success">
-                  <span class="icon-User-circle" />
-                  Approved</span
-                >
-              </td>
-              <td>
-                <span class="icon icon-Download" />
-              </td>
-            </tr>
-            <tr>
-              <td>28th Nov, 2021</td>
-              <td>2000 BDT</td>
-              <td>Basic</td>
-              <td>bKash</td>
-              <td>1254789965653</td>
-              <td>
-                <span class="status status--warning">
-                  <span class="icon-User-circle" />
-                  Pending</span
-                >
-              </td>
-              <td>
-                <span class="icon icon-Download" />
-              </td>
-            </tr>
-            <tr>
-              <td>28th Nov, 2021</td>
-              <td>2000 BDT</td>
-              <td>Basic</td>
-              <td>bKash</td>
-              <td>1254789965653</td>
-              <td>
-                <span class="status status--red">
-                  <span class="icon-User-circle" />
-                  Rejected</span
-                >
-              </td>
-              <td>
-                <span class="icon icon-Download" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    </div>
+    <ComparePlans v-else />
   </div>
 </template>
